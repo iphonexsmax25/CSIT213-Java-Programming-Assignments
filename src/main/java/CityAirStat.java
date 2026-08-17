0af3ef0f8b75847958e1e8454172394d12349b00
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-17 16:06:34 +0800

    works

--- src/main/java/CityAirStat.java
+++ src/main/java/CityAirStat.java
@@ -4,9 +4,13 @@ import java.time.*;
 import java.time.format.*;
 
 public class CityAirStat {
+    private ArrayList<AirQualityReading> readings;
+    private ArrayList<String> errors;
     
 
 	public CityAirStat() {
+            readings = new ArrayList<>();
+            errors = new ArrayList<>();
 
 	}
 
